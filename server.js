9de6d87c2dd95b57676482d3d0c1e6d61ba1ec93
Mateojlopez19 <mateojlopez@gmail.com>
2023-01-07 22:47:08 -0500

    push to git

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express(); // Instancia de la clase express
 const bodyParser = require('body-parser'); // Capturar los datos del body del frontend
 const morgan = require('morgan');
 const cors = require('cors');
+require('dotenv').config()
  //Información de peticiones server
 if(process.env.NODE_ENV != 'production'){
     require('dotenv').config();
 }
 app.set('port', process.env.PORT || 4001);
+app.set('db_name', process.env.DB_NAME|| 'merca');
 // Middlewares
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false})); // Recibir datos principales desde formulario
@@ -21,7 +23,7 @@ app.use('/api/v1/', require('./api/v1/routes/index'));
 
 
 app.get('/', (req, res) => {
-    res.send({message: 'Hello ADSI 2231424'});
+    res.send({message: 'Hello API-merca test'});
 });
 
 app.listen(app.get('port'), ()=>console.log(`server running on port ${app.get('port')}`)); // Iniciando servidor
